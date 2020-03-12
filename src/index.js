0087648440c2a6f4289300859758a815f1b18a23
Valeria <Valeryia.Karzhaneuskaya@mail.ru>
2020-03-12 23:59:12 +0300

    refactor: add solution for reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let mod = Math.abs(n);
+    let arr = mod
+        .toString()
+        .split("")
+        .reverse();
+    let result = +arr.join("");
+    return result;
+};
